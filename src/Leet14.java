b0d4de3fab1aa5f5dde80e2e4d69ceaf1080de2a
Rochellezsy <zhoushiyingzzz@gmail.com>
2020-01-14 21:14:47 +0800

    这几天做实验了，停了几天的刷题，心疼我的进度，哭唧唧

---
+++ src/Leet14.java
@@ -0,0 +1,14 @@
+public class Leet14 {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length==0) return "";
+        String str=strs[0];
+
+        for(int i=1;i<strs.length;i++){
+            while(strs[i].indexOf(str)!=0){
+                str=str.substring(0,str.length()-1);
+            }
+        }
+        return str;
+
+    }
+}
