b9d273fbfb0bbdfa6d5aa82f8c8106c212042ffb
Rochellezsy <zhoushiyingzzz@gmail.com>
2020-02-04 20:59:40 +0800

    单词搜索困难版解锁，换一种方式写，但是注意的是回溯法一定回去之后一定要把变量改回去

---
+++ src/Leet212.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leet212 {
+    public boolean back(char[][]board,String word,boolean[][]flag,int r,int c){
+        int row=board.length;
+        int col=board[0].length;
+        if(word.length()==0){
+            return true;
+        }
+        if(r<0||r>=row||c<0||c>=col||flag[r][c]==true||board[r][c]!=word.charAt(0)){
+            return false;
+        }
+        flag[r][c]=true;
+        boolean res=back(board,word.substring(1),flag,r+1,c)||back(board,word.substring(1),flag,r-1,c)||back(board,word.substring(1),flag,r,c+1)||back(board,word.substring(1),flag,r,c-1);
+        if(!res){
+            flag[r][c]=false;
+        }
+        return res;
+    }
+
+
+    public boolean exist(char[][] board, String word) {
+        int row=board.length;
+        int col=board[0].length;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(board[i][j]==word.charAt(0)){
+                    boolean[][] flag=new boolean[row][col];
+                    if(back(board,word,flag,i,j)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String >list=new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            if(exist(board,words[i])){
+                list.add(words[i]);
+            }
+        }
+        return list;
+    }
+}
