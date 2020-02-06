856910704f2d97846d0e8218b3c5dd0e610bcf38
Rochellezsy <zhoushiyingzzz@gmail.com>
2020-02-06 19:56:54 +0800

    2.6final

---
+++ src/Leet240.java
@@ -0,0 +1,21 @@
+public class Leet240 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row=matrix.length-1;
+        int col=0;
+
+        while(row>=0 && col<matrix[0].length){
+            if(matrix[row][col]==target){
+                return true;
+            }
+            if(matrix[row][col]>target){
+                row--;
+            }
+            else if(matrix[row][col]<target){
+                col++;
+            }
+        }
+
+        return false;
+
+    }
+}
