9e9c685cf7ea68e1eaffbfb3b7200d5f7b9f0176
Rochellezsy <zhoushiyingzzz@gmail.com>
2020-02-08 21:40:01 +0800

    Leet50

---
+++ src/Leet50.java
@@ -0,0 +1,11 @@
+public class Leet50 {
+    public double myPow(double x, int n) {
+        if(n==0)return 1;
+        if(n==1)return x;
+        if(n==-1)return 1/x;
+        double half=myPow(x,n/2);
+        double res=myPow(x,n%2);
+        return half*half*res;
+
+    }
+}
