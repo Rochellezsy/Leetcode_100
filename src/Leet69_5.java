4b21ac1d5a751642562c7a501a8df25081bcd544
Rochellezsy <zhoushiyingzzz@gmail.com>
2020-02-08 21:43:21 +0800

    平方根

---
+++ src/Leet69_5.java
@@ -0,0 +1,10 @@
+public class Leet69_5 {
+    public int mySqrt(int x) {
+        long res=x;
+        while(res*res>x){
+            res=(res+x/res)/2;
+        }
+        return (int)res;
+
+    }
+}
