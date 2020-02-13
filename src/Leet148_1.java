5295b1bdb2fdfb35b28ac19405b991cc46b6f6d9
Rochellezsy <zhoushiyingzzz@gmail.com>
2020-02-13 20:46:36 +0800

    指针排序，归并算法

---
+++ src/Leet148_1.java
@@ -0,0 +1,43 @@
+public class Leet148_1 {
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null)return null;
+        ListNode fast=head.next;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        ListNode temp=slow.next;
+        slow.next=null;
+        ListNode left=sortList(head);
+        ListNode right=sortList(temp);
+
+        return merge(left,right);
+    }
+
+    public ListNode merge(ListNode l1,ListNode l2){
+        ListNode res=new ListNode(0);
+        ListNode p=res;
+        while(l1!=null && l2!=null){
+            if(l1.val<=l2.val){
+                p.next=l1;
+                l1=l1.next;
+            }
+            else {
+                p.next=l2;
+                l2=l2.next;
+            }
+            p=p.next;
+        }
+        if(l1==null){
+            p.next=l2;
+        }
+        if(l2==null){
+            p.next=l1;
+        }
+        return res.next;
+
+    }
+
+
+}
